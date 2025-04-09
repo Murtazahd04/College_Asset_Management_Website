384ac8c6bf30db2797b40776d6b0730ccd8f2d38
Murtaza Huzefa Dhanerawala <106521144+Murtazahd04@users.noreply.github.com>
2025-04-09 19:39:41 +0530

    Add files via upload

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path , include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.homepage),
+    path('about/',views.about),
+    path('posts/',include('posts.urls')),
+    path('college_asset/',include('college_asset.urls')),
+    path('college/', include('college_asset.urls')),
+]
