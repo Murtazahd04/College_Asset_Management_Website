3c994a544285dbe8c793526225671a2926618f16
Murtaza Huzefa Dhanerawala <106521144+Murtazahd04@users.noreply.github.com>
2025-04-11 02:25:24 +0530

    Delete myproject directory

--- myproject/college_asset/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import RegisteredAsset, Classroom, ClassroomAsset
-
-admin.site.register(RegisteredAsset)
-admin.site.register(Classroom)
-admin.site.register(ClassroomAsset)
